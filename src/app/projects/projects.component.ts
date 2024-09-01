1bf463757e8d07daf7f5e9661441f2fd9021dd85
Aftab Alam <theaftabalam@admins-MacBook-Pro.local>
2024-09-02 00:54:38 +0530

    links update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -17,15 +17,15 @@ export class ProjectsComponent implements OnInit {
       description: 'A comprehensive system to manage patient records, appointments, and billing. Includes features for staff management, medical history tracking, and integration with third-party healthcare APIs.',
       image: 'assets/healthcare.png',
       link: 'https://example.com/healthcare-management-system',
-      source: 'https://github.com/user/healthcare-management-system',
+      source: 'https://github.com/TheAftabAlam/hms',
       techStack: ['Angular', 'Spring Boot', 'Java', 'MySQL', 'REST API', 'JWT', 'Angular Material']
     },    
     {
       title: 'Ahaar POS',
       description: 'Streamline your sales, order processing, and kitchen operations effortlessly with HB Aahar POS system. Integrated Swiggy and Zomato using Urban Piper platform, expanding project reach',
       image: 'assets/pos.png',
-      link: 'https://example.com/aahaar-pos',
-      source: 'https://github.com/user/aahaar-pos',
+      link: 'https://play.google.com/store/apps/details?id=hostbooks.com.aahar&hl=en_IN',
+      source: 'https://play.google.com/store/apps/details?id=hostbooks.com.aahar&hl=en_IN',
       techStack: ['Java', 'Spring Boot', 'Firebase', 'MySQL', 'SqlLite', 'Hiberante','Spring Security', 'Web Sockets']
     },
     {
@@ -33,15 +33,15 @@ export class ProjectsComponent implements OnInit {
       description: 'HB Super App is a comprehensive business management solution developed by HostBooks Limited. Contributed to the development of a comprehensive business management solution.',
       image: 'assets/hb-super-app.png',
       link: 'https://example.com/hb-super-app',
-      source: 'https://github.com/user/hb-super-app',
+      source: 'https://play.google.com/store/apps/details?id=hostbooks.com.hbsuper&hl=en_IN',
       techStack:['Java', 'Spring Boot', 'MySQL', 'Spring Security','Hiberante','Angular','Css','Html']
     },
     {
       title: 'Cricbuzz Clone',
       description: 'A clone of the popular Cricbuzz platform for live cricket scores and updates. Includes features for live score updates, match schedules, and player statistics.',
       image: 'assets/cricbuzz-clone.png',
-      link: 'https://example.com/cricbuzz-clone',
-      source: 'https://github.com/user/cricbuzz-clone',
+      link: 'https://luxury-douhua-2f73cb.netlify.app/',
+      source: 'https://github.com/TheAftabAlam/CricBuzz',
       techStack: ['JavaScript', 'Node.js', 'Html', 'Css']
     },
     {
@@ -49,7 +49,7 @@ export class ProjectsComponent implements OnInit {
       description: 'A system for managing nursery plants, including inventory, growth tracking, and sales management. Features include plant care schedules, sales reporting, and customer management.',
       image: 'assets/nursery-plant-management.png',
       link: 'https://example.com/nursery-plant-management',
-      source: 'https://github.com/user/nursery-plant-management',
+      source: 'https://github.com/TheAftabAlam/Nursary-Plant-Api',
       techStack: ['Spring', 'SpringBoot', 'Spring Security', 'Java', 'MySql']
     },
     {
@@ -57,7 +57,7 @@ export class ProjectsComponent implements OnInit {
       description: 'A comprehensive system for managing employee records, payroll, and performance evaluations. Includes features for tracking attendance, leaves, and employee data management.',
       image: 'assets/employee-management-system.jpg',
       link: 'https://example.com/employee-management-system',
-      source: 'https://github.com/user/employee-management-system',
+      source: 'https://github.com/TheAftabAlam/EmployeeManagement-Springboot-Angular-',
       techStack: ['Java', 'Spring Boot', 'MySQL', 'Angular']
     }
   //   {
